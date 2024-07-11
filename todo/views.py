89d704bfffd24ec375255bd17783110b5fc2ff83
1F10230058 <s1f102300582@iniad.org>
2024-07-11 14:39:07 +0900

    修正

--- todo/views.py
+++ todo/views.py
@@ -36,7 +36,7 @@ def detail(request, task_id):
 
 def update(request, task_id):
     try:
-        task = Task.objects.get(pk=task)
+        task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
     if request.method == "POST":
